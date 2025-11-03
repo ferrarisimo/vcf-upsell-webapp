ad42d74c323fc39407fbc7f1963617cc92e77d66
Simone Ferrari <ferrari.simo@gmail.com>
2025-11-03 18:11:43 +0100

    Fix: clean globals.css (remove stray JS code)

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-// Importa le pagine principali
 import Home from './pages/Home'
 import Assessment from './pages/Assessment'
 import Thanks from './pages/Thanks'
-
-// Importa gli stili globali Tailwind
 import './styles/globals.css'
 
-// Definizione delle route principali
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/assessment',
-    element: <Assessment />,
-  },
-  {
-    path: '/grazie',
-    element: <Thanks />,
-  },
+  { path: '/', element: <Home/> },
+  { path: '/assessment', element: <Assessment/> },
+  { path: '/grazie', element: <Thanks/> },
 ])
 
-// Rendering dell'applicazione React
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
